b854a4e2be33f9681c889e6335d4d3e582102ee0
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2022-07-01 16:45:16 +0800

    Initial UI for Homepage * Added the form for Dashboard login and Survey Creation * Also added the related SCSS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { getContext } from 'svelte';
+
+    let pageName = getContext('pageName');
+    $pageName = 'Home';
+</script>
+
+<div class="container-main">
+    <div class="header">
+        <div class="header__logo">
+            <h4>Welcome to</h4>
+            <h2>Motivation Meter</h2>
+        </div>
+        <button class="btn btn__header">
+            <i class="fas fa-file-alt"></i>
+            Answer a survey
+        </button>
+    </div>
+
+    <div class="main-content">
+        <div class="home-card">
+            <!-- Add create survey functionality to the button -->
+            <button class="btn btn__primary">
+                <i class="fas fa-plus"></i>
+                Create a Motivation Survey
+            </button>
+            <div class="card__separator">
+                <hr/>
+                <p>or</p>
+                <hr/>
+            </div>
+
+            <!-- Add error message div -->
+            <!-- Add information message div -->
+
+            <!-- Add form functionality -->
+            <form class="form__dashboard-login">
+                <input
+                    type="text"
+                    class="form__input form__input--survey-id"
+                    placeholder="Enter provided Survey ID"
+                />
+                <!-- Add eye feature -->
+                <div class="form__group form__input--dashboard-password">
+                    <input 
+                        type="password"
+                        placeholder="Enter registered password"
+                    >
+                    <i 
+                        class="fas fa-eye cursor--pointer"
+                    ></i>
+                </div>
+                <button class="btn btn__primary btn--create-survey">
+                    <i class="fas fa-sign-in-alt"></i>
+                    Login and Manage Survey
+                </button>
+            </form>
+        </div>
+        <!-- Add motivation quote section -->
+
+    </div>
+</div>
