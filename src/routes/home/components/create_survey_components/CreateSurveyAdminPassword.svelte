f04d3b40ea4165e6867ae6d6e0c33bd6b126fda5
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2022-07-06 18:46:25 +0800

    Added keystroke based validation to entered password and confirmation * Added regex checking, character count, and equal value with entered confirmatory password * Added displayed error messages and dynamic display for current accomplishment of the validation

--- src/routes/home/components/create_survey_components/CreateSurveyAdminPassword.svelte
+++ src/routes/home/components/create_survey_components/CreateSurveyAdminPassword.svelte
@@ -1,13 +1,82 @@
 <script>
-    import { getContext, onMount } from 'svelte'
+    import { getContext, onMount, createEventDispatcher } from 'svelte'
+    import { surveySettings } from '../../create_survey_store';
 
     export let animationToExecute;
+    export let displayError;
+
+    const minimumCharactersPassword = 6;
+    const passwordCriteria = [
+        {
+            description: `Must be at least ${minimumCharactersPassword} characters.`,
+            accomplished: false
+        },
+        {
+            description: `
+                Must contain: 
+                    <br/>&emsp;&emsp;&#8226; at least 1 letter
+                    <br/>&emsp;&emsp;&#8226; at least 1 number, and
+                    <br/>&emsp;&emsp;&#8226; at least 1 of the allowed special characters (!?@#$%^&)
+            `,
+            accomplished: false
+        }
+    ];
+    const dispatch = createEventDispatcher();
+    
+    // Ensures password has at least 1 number, 1 letter, and 1 of the only allowed set of special characters.
+    const regexValidPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&])([a-zA-Z0-9!?@#$%^&]+)$/;
 
     let formElement;
     let registeredDashboardPassword = '';
+    let confirmatoryDasboardPassword = '';
+    let confirmatoryDasboardPasswordIsSame = false;
     let doAnimation = getContext('doAnimation');
 
     $: hasEnteredPassword = registeredDashboardPassword !== '';
+
+    $: if (registeredDashboardPassword === '') {
+        confirmatoryDasboardPassword = '';
+    }
+
+    $:  if (
+            registeredDashboardPassword !== '' && 
+            registeredDashboardPassword.length >= minimumCharactersPassword
+        ) {
+            passwordCriteria[0].accomplished = true;
+        } else {
+            passwordCriteria[0].accomplished = false;
+        }
+
+    $: passwordHasValidRegexPattern = regexValidPassword.test(registeredDashboardPassword);
+
+    $: if (passwordHasValidRegexPattern) {
+        passwordCriteria[1].accomplished = true;
+    } else {
+        passwordCriteria[1].accomplished = false;
+    }
+
+    $: if (hasEnteredPassword && registeredDashboardPassword === confirmatoryDasboardPassword) {
+        confirmatoryDasboardPasswordIsSame = true;
+    } else {
+        confirmatoryDasboardPasswordIsSame = false;
+    }
+
+    $: passwordIsValid = 
+        registeredDashboardPassword.length >= minimumCharactersPassword &&
+        passwordHasValidRegexPattern && 
+        confirmatoryDasboardPasswordIsSame
+    
+    $: dispatch('message', { hasAnswer: passwordIsValid });
+
+    $: if (passwordIsValid) {
+        $surveySettings.adminPassword = registeredDashboardPassword;
+    }
+
+    function removeError() {
+        if (displayError) {
+            displayError = false;
+        }
+    }
 
     onMount(() => {
         if (animationToExecute !== null && animationToExecute.fade === 'fadeIn') {
@@ -52,6 +121,7 @@
                     type="password"
                     placeholder="Enter password"
                     bind:value={registeredDashboardPassword}
+                    on:keydown={removeError}
                 >
                 <i 
                     class="fas fa-eye cursor--pointer"
@@ -62,8 +132,60 @@
                     }}
                 ></i>
             </div>
+
             <!-- Add error displayed for the registered password -->
+            {#if hasEnteredPassword}
+            <ul>
+                {#each passwordCriteria as criterion}
+                    <li 
+                        class:form__input_message--success={criterion.accomplished} 
+                        class:form__input_message--error={!criterion.accomplished}
+                    >
+                        <i class="fas" class:fa-check={criterion.accomplished} class:fa-times={!criterion.accomplished}></i>
+                        {@html criterion.description}
+                    </li>
+                {/each}
+            </ul>
+            {/if}
+
         </div>
-        <input type="text" class="form__input" placeholder="Confirm entered password">
+        <div class="create-survey-modal__form-input-group">
+            <div 
+                class="create-survey-modal__form-input-field"
+                class:create-survey-modal__form-input-field--disabled={!hasEnteredPassword}
+            >
+                <input 
+                    type="password"
+                    placeholder="Confirm entered password" 
+                    disabled={!hasEnteredPassword}
+                    bind:value={confirmatoryDasboardPassword}
+                    on:keydown={removeError}
+                >
+                <i 
+                    class="fas fa-eye cursor--pointer"
+                    on:click={(e) => {
+                        if (hasEnteredPassword) {
+                            toggleFieldDisplay(e)
+                        }
+                    }}
+                ></i>
+            </div>
+
+            {#if confirmatoryDasboardPassword !== ''}
+            <ul>
+                <li
+                    class:form__input_message--success={confirmatoryDasboardPasswordIsSame} 
+                    class:form__input_message--error={!confirmatoryDasboardPasswordIsSame}
+                >
+                    <i
+                        class="fas"
+                        class:fa-check={confirmatoryDasboardPasswordIsSame}
+                        class:fa-times={!confirmatoryDasboardPasswordIsSame}
+                    ></i>
+                    Must be the same as the previously entered password.
+                </li>
+            </ul>
+            {/if}
+        </div>
     </div>
 </div>
