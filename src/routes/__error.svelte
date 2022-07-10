2197ce279122ac61eaa37d790337ca764195eb6b
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2022-07-10 19:10:41 +0800

    Added the proper UI to the general error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+    export function load({ error, status }) {
+        console.error(error.message, error);
+        return {
+            props: {
+                title: status,
+                message: error.message
+            }
+        };
+    }
+</script>
+
+<script>
+    import ErrorView from "../common_components/ErrorView.svelte";
+
+    export let title;
+    export let message;
+    
+    let isSystemError = false;
+
+    $: if (title == '404') {
+        title = 'Page not found.';
+        message = 'The requested page does not exist.';
+    }
+</script>
+
+<ErrorView
+    isSystemError={isSystemError}
+    errorTitle={title}
+    errorMessage={message}
+/>
