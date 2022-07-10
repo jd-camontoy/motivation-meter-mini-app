8727f9e9508f2caec18b3d6a817e2b0fabd96b4b
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2022-07-10 15:50:17 +0800

    Initial UI and route for Survey * Created a new component for Survey that also accepts a token as a URL parameter

---
+++ src/routes/survey/[token].svelte
@@ -0,0 +1,13 @@
+<div class="survey-card">
+    <div class="survey-card__header">
+        <h1>Motivation Meter Survey</h1>
+        <h2>for February 22, 2022</h2>
+    </div>
+    <p class="survey-card__description margin-bottom-60">
+        This survey serves as an avenue for employees to express their overall feelings when regards to their motivation at work. It also serves as an avenue for superviors and managers to know overall motivation levels of their subordinates.
+    </p>
+    <button class="btn btn__primary btn--start-survey">
+        <i class="fas fa-play"></i>
+        Start the Survey
+    </button>
+</div>
