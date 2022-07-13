22cabe9f2d6356f671d676a0f6f46e2e81c6cdaa
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2022-07-13 13:02:02 +0800

    Added functionality to the Survey Access modal * Added validations to the entered token

--- src/routes/home/components/SurveyAccessModal.svelte
+++ src/routes/home/components/SurveyAccessModal.svelte
@@ -3,8 +3,41 @@
     import { fade, scale } from 'svelte/transition';
 
     const animationSpeed = 100;
+    const validSurveyTokenRegex = /^[0-9A-Za-z]+$/;
+
+    let displayedErrorMsg;
+    let displayError = false;
+    let enteredSurveyToken = '';
+
+    let pageName = getContext('pageName');
+    $pageName = 'Answer a Survey';
 
     let hideSurveyModal = getContext('hideSurveyModal');
+
+    let displayErrorTimeout;
+
+    $: if (displayError) {
+        displayErrorTimeout = setTimeout(() => {
+            displayError = false;
+        }, 5000);
+    } else {
+        clearTimeout(displayErrorTimeout)
+    }
+    
+    $: surveyUrl = '/survey/' + enteredSurveyToken;
+
+    function validateEnteredToken() {
+        if (enteredSurveyToken === '') {
+            displayError = true;
+            displayedErrorMsg = 'Please provide the survey token.';
+        } else if (!validSurveyTokenRegex.test(enteredSurveyToken)) {
+            displayError = true;
+            displayedErrorMsg = 'Please provide a valid survey token.';
+        } else {
+            window.open(surveyUrl,'_blank');
+            hideSurveyModal();
+        }
+    }
 </script>
 
 <div class="create-survey-modal">
@@ -26,14 +59,24 @@
                 <h1>Let's answer a survey</h1>
                 <h2>Kindly enter the provided survey token on the field below.</h2>
             </div>
-            <form class="form__dashboard-login margin-top-40">
-                <!-- Add eye feature -->
+
+            {#if displayError}
+                <div class="note note--error">
+                    {displayedErrorMsg}
+                </div>
+            {/if}
+
+            <form class="form__dashboard-login margin-top-40" on:submit|preventDefault={validateEnteredToken}>
                 <input
+                    bind:value={enteredSurveyToken}
+                    on:input={() => { 
+                        displayError = false;
+                    }}
                     type="text"
                     class="form__input form__input--survey-id"
                     placeholder="Enter provided Survey ID"
                 />
-                <button class="btn btn__primary">
+                <button class="btn btn__primary" type="submit">
                     <i class="fas fa-sign-in-alt"></i>
                     Access the Survey
                 </button>
